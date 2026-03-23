c65a11778f1ae3166a992d15a6cc6912c0ac3858
vivek <vivan100123@gmail.com>
2026-03-24 01:38:14 +0530

    Set up the project

---
+++ src/main/java/com/vivek/smart_debug_starter/SmartDebugStarterApplication.java
@@ -0,0 +1,12 @@
+package com.vivek.smart_debug_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDebugStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDebugStarterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vivek/smart_debug_starter/SmartDebugStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.smart_debug_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDebugStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
